f9e66ee4b31aca3270f8d9513807a777eb3af4e5
sshusshu <subin9033@gmail.com>
2021-06-10 14:58:31 +0900

    sshu: slider js 오류 수정중 1

--- basic/slider.js
+++ basic/slider.js
@@ -123,9 +123,11 @@ var Slider = (function (){
         }
     }
     let getPosition = function(e){
-        console.dir(e.originalEvent)
+
         if (e.type.split('touch').length > 1) {
-            return {x:e.originalEvent.touches[0].clientX, y:e.originalEvent.touches[0].clientY}
+            console.dir(e)
+            console.dir(e.originalEvent)
+            //return {x:e.originalEvent.touches[0].clientX, y:e.originalEvent.touches[0].clientY}
         }
         return {x:e.clientX, y:e.clientY};
     }
